503599b1e611dae6305507f44111a6edd075c58a
Bryan Guzman Valerin <bdgv11@gmail.com>
2022-12-22 22:37:58 -0600

    Adding next appointment widget to homePage

--- barber/lib/feature_home/screens/home_page.dart
+++ barber/lib/feature_home/screens/home_page.dart
@@ -4,6 +4,7 @@ import 'package:barber/feature_home/widgets/bottom_navigation.dart';
 import 'package:barber/feature_home/widgets/drawer_widget.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class HomePageScreen extends StatefulWidget {
@@ -21,12 +22,43 @@ class _MyWidgetState extends State<HomePageScreen> {
   @override
   void initState() {
     _user = widget.user;
+    Firebase.initializeApp();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     final double width = MediaQuery.of(context).size.width;
+    print('DateTime.Now == ${DateTime.now()}');
+    print('TimeStamp.Now == ${Timestamp.now()}');
+    print('DateTime.Now,milisse == ${DateTime.now().millisecondsSinceEpoch}');
+
+    String getFormattedDate(String year, String month, String day) {
+      String fecha;
+      String m = '';
+      String d = '';
+      if (month.length == 1) {
+        m = '0$month';
+      } else {
+        m = month;
+      }
+      if (day.length == 1) {
+        d = '0$day';
+      } else {
+        d = day;
+      }
+      fecha = '$year-$m-$d';
+      return fecha;
+    }
+
+    String fecha = getFormattedDate(DateTime.now().year.toString(),
+        DateTime.now().month.toString(), DateTime.now().day.toString());
+
+    DateTime dateTimeFecha = DateTime.parse(fecha);
+    dateTimeFecha.millisecondsSinceEpoch;
+    print(Timestamp.fromDate(dateTimeFecha));
+
+    print('Este es === $dateTimeFecha');
 
     return Scaffold(
       appBar: AppBar(backgroundColor: Colors.black87),
@@ -59,6 +91,106 @@ class _MyWidgetState extends State<HomePageScreen> {
                       fontWeight: FontWeight.bold),
                 ),
               ],
+            ),
+            const Divider(
+              thickness: 0.2,
+              color: Colors.white,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: const [
+                Text(
+                  'Mi proxima cita:',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Barlow'),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 180,
+              child: StreamBuilder(
+                stream: FirebaseFirestore.instance
+                    .collection('Cita')
+                    .where('Cliente', isEqualTo: _user.displayName.toString())
+                    .where('Fecha',
+                        isGreaterThanOrEqualTo:
+                            DateTime.now().millisecondsSinceEpoch)
+                    .limit(1)
+                    .snapshots(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: snapshot.data!.docs.length,
+                      itemBuilder: (context, index) {
+                        final DocumentSnapshot documentSnapshot =
+                            snapshot.data!.docs[index];
+
+                        DateTime fechaDesdeBD =
+                            DateTime.fromMillisecondsSinceEpoch(
+                                documentSnapshot['Fecha']
+                                    .millisecondsSinceEpoch);
+                        print('Fecha desde DB $fechaDesdeBD');
+                        print(documentSnapshot['Fecha']);
+
+                        String fechaFormateada =
+                            '${fechaDesdeBD.day.toString()}/${fechaDesdeBD.month.toString()}/${fechaDesdeBD.year.toString()}';
+
+                        return FadeIn(
+                          delay: Duration(milliseconds: 200 * index),
+                          child: ListTile(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            leading: const Icon(
+                              Icons.apple_outlined,
+                              color: Colors.white70,
+                              //size: 50,
+                            ),
+                            title: Text(
+                              fechaFormateada,
+                              style: const TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: 'Barlow',
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20),
+                            ),
+                            subtitle: Text(
+                              documentSnapshot['TipoServicio'],
+                              style: const TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: 'Barlow',
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20),
+                            ),
+                            trailing: Text(
+                              documentSnapshot['Hora'],
+                              style: const TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: 'Barlow',
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 25),
+                            ),
+                            onTap: () {
+                              setState(() {});
+                            },
+                          ),
+                        );
+                      },
+                    );
+                  }
+                  return const CircularProgressIndicator();
+                },
+              ),
             ),
             const Divider(
               thickness: 0.2,
