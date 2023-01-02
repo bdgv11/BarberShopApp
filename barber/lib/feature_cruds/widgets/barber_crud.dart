38f12cb1ac6e2974ab437586aef787d22535d286
Bryan Guzman Valerin <bdgv11@gmail.com>
2023-01-01 22:16:44 -0600

    Modifying barber crud view

--- barber/lib/feature_cruds/widgets/barber_crud.dart
+++ barber/lib/feature_cruds/widgets/barber_crud.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import '../../utils/validator.dart';
@@ -124,12 +125,141 @@ class _BarberCrudState extends State<BarberCrud> {
                         ),
                         const Padding(padding: EdgeInsets.all(8)),
                         SizedBox(
+                          height: 400,
+                          child: StreamBuilder(
+                            stream: FirebaseFirestore.instance
+                                .collection('Barbero')
+                                .where('Disponible', isEqualTo: true)
+                                .snapshots(),
+                            builder: (BuildContext context,
+                                AsyncSnapshot<QuerySnapshot> snapshot) {
+                              if (snapshot.connectionState ==
+                                  ConnectionState.waiting) {
+                                return const Center(
+                                  child: CircularProgressIndicator(),
+                                );
+                              }
+
+                              if (snapshot.hasData) {
+                                return ListView.builder(
+                                  itemCount: snapshot.data!.docs.length,
+                                  itemBuilder: (context, index) {
+                                    final DocumentSnapshot documentSnapshot =
+                                        snapshot.data!.docs[index];
+
+                                    return /*Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceEvenly,
+                                        children: [
+                                          CircleAvatar(
+                                            backgroundImage: NetworkImage(
+                                              documentSnapshot['ImagenURL'],
+                                            ),
+                                            radius: 30,
+                                          ),
+                                          Text(
+                                            documentSnapshot['Nombre'],
+                                            style: const TextStyle(
+                                                color: Colors.white,
+                                                fontFamily: 'Barlow',
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 20),
+                                          ),
+                                          Text(
+                                            documentSnapshot['Descripcion'],
+                                            style: const TextStyle(
+                                                color: Colors.white,
+                                                fontFamily: 'Barlow',
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 20,
+                                                overflow:
+                                                    TextOverflow.ellipsis),
+                                          ),
+                                          IconButton(
+                                            icon: const Icon(
+                                              Icons.edit_outlined,
+                                              size: 30,
+                                            ),
+                                            color: Colors.white,
+                                            onPressed: () {
+                                              _deleteBarber(documentSnapshot.id,
+                                                  documentSnapshot['Nombre']);
+                                            },
+                                          ),
+                                          IconButton(
+                                            icon: const Icon(
+                                              Icons.delete_outline_outlined,
+                                              size: 30,
+                                            ),
+                                            color: Colors.white,
+                                            onPressed: () {
+                                              _deleteBarber(documentSnapshot.id,
+                                                  documentSnapshot['Nombre']);
+                                            },
+                                          ),
+                                        ],
+                                      ),
+                                    );*/
+                                        ListTile(
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(20),
+                                      ),
+                                      leading: CircleAvatar(
+                                        backgroundImage: NetworkImage(
+                                          documentSnapshot['ImagenURL'],
+                                        ),
+                                        radius: 30,
+                                      ),
+                                      title: Text(
+                                        documentSnapshot['Nombre'],
+                                        style: const TextStyle(
+                                            color: Colors.white,
+                                            fontFamily: 'Barlow',
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 20),
+                                      ),
+                                      subtitle: Text(
+                                        documentSnapshot['Descripcion'],
+                                        style: const TextStyle(
+                                            color: Colors.white,
+                                            fontFamily: 'Barlow',
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 20,
+                                            overflow: TextOverflow.ellipsis),
+                                      ),
+                                      isThreeLine: true,
+                                      dense: true,
+                                      trailing: IconButton(
+                                        icon: const Icon(
+                                          Icons.delete_outline_outlined,
+                                          size: 35,
+                                        ),
+                                        color: Colors.white,
+                                        onPressed: () {
+                                          _deleteBarber2(
+                                            documentSnapshot.id,
+                                            documentSnapshot['Nombre'],
+                                          );
+                                        },
+                                      ),
+                                    );
+                                  },
+                                );
+                              }
+                              return const CircularProgressIndicator();
+                            },
+                          ),
+                        ),
+                        const Padding(padding: EdgeInsets.all(8)),
+                        SizedBox(
                           height: 150,
                           child: Container(
                             width: 200,
                             decoration: BoxDecoration(
                               //borderRadius: BorderRadius.circular(20),
-                              border: Border.all(color: Colors.white),
+                              border: Border.all(color: Colors.white38),
                             ),
                             child: Center(
                               child: Column(
@@ -252,18 +382,6 @@ class _BarberCrudState extends State<BarberCrud> {
                                                 'Seleccione una imagen',
                                                 const Duration(seconds: 2));
                                           }
-
-                                          /// Trying to log in the user using the email and password that
-                                          /// the user has entered.
-                                          /*User? user =
-                                              await FirebaseAuthentication
-                                                  .signInUsingEmailAndPassword(
-                                                      email: _emailFieldController
-                                                          .text
-                                                          .trim(),
-                                                      password:
-                                                          _passwordFieldController
-                                                              .text);*/
                                         }
                                       },
                                       child: const Text(
@@ -278,6 +396,7 @@ class _BarberCrudState extends State<BarberCrud> {
                                   ),
                                 ],
                               ),
+                        const Divider(),
                       ],
                     ),
                   )
@@ -289,4 +408,53 @@ class _BarberCrudState extends State<BarberCrud> {
       ),
     );
   }
+}
+
+Widget _deleteBarber(String id, String nombre, BuildContext context) {
+  return CupertinoAlertDialog(
+    title: const Text(
+      '¿Desea eliminar el barbero?',
+      style: TextStyle(
+          fontFamily: 'Barlow', fontWeight: FontWeight.w900, fontSize: 20),
+    ),
+    // ignore: prefer_const_constructors
+    content: Text(
+      'Podria perderse informacion de citas...',
+      style: const TextStyle(
+          fontFamily: 'Barlow', fontWeight: FontWeight.w900, fontSize: 20),
+    ),
+    actions: [
+      TextButton(
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+        child: const Text(
+          'Cancelar',
+          style: TextStyle(color: Colors.red),
+        ),
+      ),
+      TextButton(
+        onPressed: () {
+          // This method will delete the barber
+          FirebaseFirestore.instance.collection('Barbero').doc(id).delete();
+
+          FirebaseStorage.instance.ref('Barbero').child(nombre).delete();
+          Navigator.of(context).pop();
+        },
+        child: const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text(
+            'Agendar',
+          ),
+        ),
+      ),
+    ],
+  );
+}
+
+void _deleteBarber2(String id, String nombre) {
+  // This method will delete the barber
+  FirebaseFirestore.instance.collection('Barbero').doc(id).delete();
+
+  FirebaseStorage.instance.ref('Barbero').child(nombre).delete();
 }
